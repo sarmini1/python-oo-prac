eb868cd6b3c0c9e76773d42c793a5df896cded79
sarmini1 <spencerarmini@gmail.com>
2021-04-20 17:38:37 -0700

    finished randomwordfinder subclass

--- wordfinder.py
+++ wordfinder.py
@@ -1,16 +1,18 @@
 from random import choice
 
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
-    #__init__
-        # pass file path open file function (filepath, r)
-        # make list of words from file
-        # save length of list as count of words
+    #__init__ (self,filepath)
+        # runs word_clean_up method and saves result to words variable
         # Return "'count' words read"
     
-    # def random()
+    # def random(self)
         # use imported choice function to pick a word from the list and return it
+    
+    # def word_clean_up(self,filepath)
+        #returns new list removing end line characters from file object words
 
     def __init__(self, filepath):
         """Accepts a filepath to a text file containing words"""
@@ -19,10 +21,22 @@ class WordFinder:
 
     def random(self):
         """Returns a random word from the file"""
-        
         return choice(self.words)
         
     def word_clean_up(self, filepath):
-        """removes new linecharacters from file object"""
-        words = list(open(filepath, 'r'))
-        return [word[0: len(word) - 1] for word in words]
+        """removes new line characters from words"""
+        return [word[0: len(word) - 1] for word in open(filepath, 'r')]
+
+#class RandomWordFinder(WordFinder):
+
+    #def word_clean_up(self,filepath):
+        #return list comprehension filtering out lines that
+        # start with hashtags or spaces
+
+
+class RandomWordFinder(WordFinder):
+    """Finds valid words from file"""
+    def word_clean_up(self, filepath):
+        """ Removes comments and blank spaces, returns filtered list of words """
+        return [word for word in super().word_clean_up(filepath)
+                if not word.startswith("#") and not word == ""]
